2d72ae6d3411b2f7ba4702dab1a4d32bcb6e8fdd
“mdreu” <mdreusi@gmail.com>
2022-03-01 09:53:39 -0500

    HW

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(0);
+//Numbers, strings, null, Booleans,undefined
+// + - * % / **
+console.log(5%4);
+console.log(5%2);
+console.log('I\'M MARY POPPINS Y\'ALL');
+console.log("Hawk" + "eye");
+console.log("Matthew".length + "Dreusicke".length);
+console.log("Avengers: Endgame"[16]);
